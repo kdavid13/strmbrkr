a7c3d559b2fa93e46e0ce54440fa051f25d653d6
David Kusterer <kdavid13@vt.edu>
2024-10-04 15:17:15 -0400

    update kvs cache tests to use `KeyValueStore` methods rather than `Transaction`s directly

--- tests/test_kvs_cache.py
+++ tests/test_kvs_cache.py
@@ -5,9 +5,6 @@ import pytest
 # Package Imports
 from strmbrkr import KeyValueStore
 from strmbrkr.key_value_store.cache_transactions import (
-    InitCache,
-    CachePut,
-    CacheGrab,
     CacheMiss,
     UninitializedCache,
     ValueAlreadySet
@@ -20,70 +17,55 @@ VALUE = "my_value"
 
 
 def test_initCache(kvs_server_teardown):
-    init_cache = InitCache(CACHE_NAME)
-    assert KeyValueStore.submitTransaction(init_cache)
+    assert KeyValueStore.initCache(CACHE_NAME)
 
 
 def test_duplicateInitCache(kvs_server_teardown):
-    init_cache = InitCache(CACHE_NAME)
-    assert KeyValueStore.submitTransaction(init_cache)
+    assert KeyValueStore.initCache(CACHE_NAME)
 
     with pytest.raises(ValueAlreadySet, match="Value already set for key"):
-        KeyValueStore.submitTransaction(init_cache)
+        KeyValueStore.initCache(CACHE_NAME)
 
-    init_cache.clear_existing = True
-    assert KeyValueStore.submitTransaction(init_cache)
+    assert KeyValueStore.initCache(CACHE_NAME, clear_existing=True)
 
 
 def test_cachePutGrab(kvs_server_teardown):
-    init_cache = InitCache(CACHE_NAME)
-    assert KeyValueStore.submitTransaction(init_cache)
+    assert KeyValueStore.initCache(CACHE_NAME)
 
-    cache_put = CachePut(CACHE_NAME, KEY, VALUE)
-    assert KeyValueStore.submitTransaction(cache_put)
+    assert KeyValueStore.cachePut(CACHE_NAME, KEY, VALUE)
 
-    cache_grab = CacheGrab(CACHE_NAME, KEY)
-    grabbed = KeyValueStore.submitTransaction(cache_grab)
-
+    grabbed = KeyValueStore.cacheGrab(CACHE_NAME, KEY)
     assert grabbed == VALUE
 
 
 def test_noCachePut(kvs_server_teardown):
-    cache_put = CachePut(CACHE_NAME, KEY, VALUE)
     with pytest.raises(UninitializedCache):
-        KeyValueStore.submitTransaction(cache_put)
+        KeyValueStore.cachePut(CACHE_NAME, KEY, VALUE)
 
 
 def test_noCacheGrab(kvs_server_teardown):
-    cache_grab = CacheGrab(CACHE_NAME, KEY)
     with pytest.raises(UninitializedCache):
-        KeyValueStore.submitTransaction(cache_grab)
+        KeyValueStore.cacheGrab(CACHE_NAME, KEY)
     
 
 def test_cacheMiss(kvs_server_teardown):
-    cache_init = InitCache(CACHE_NAME)
-    KeyValueStore.submitTransaction(cache_init)
+    assert KeyValueStore.initCache(CACHE_NAME)
 
-    cache_grab = CacheGrab(CACHE_NAME, KEY)
     with pytest.raises(CacheMiss):
-        KeyValueStore.submitTransaction(cache_grab)
+        KeyValueStore.cacheGrab(CACHE_NAME, KEY)
 
 
 def test_cacheSize(kvs_server_teardown):
     cache_size = 16
-    cache_init = InitCache(CACHE_NAME, max_size=cache_size)
-    assert KeyValueStore.submitTransaction(cache_init)
+    KeyValueStore.initCache(CACHE_NAME, max_size=cache_size)
 
     for index in range(cache_size * 2):
-        cache_put = CachePut(CACHE_NAME, f"id_{index}", index)
-        assert KeyValueStore.submitTransaction(cache_put)
+        assert KeyValueStore.cachePut(CACHE_NAME, f"id_{index}", index)
 
     for index in range(cache_size):
-        cache_grab = CacheGrab(CACHE_NAME, f"id_{index}")
         with pytest.raises(CacheMiss):
-            KeyValueStore.submitTransaction(cache_grab)
+            KeyValueStore.cacheGrab(CACHE_NAME, f"id_{index}")
 
         upper_index = cache_size + index
-        cache_grab = CacheGrab(CACHE_NAME, f"id_{upper_index}")
-        assert KeyValueStore.submitTransaction(cache_grab) == upper_index
+        assert KeyValueStore.cacheGrab(CACHE_NAME, f"id_{upper_index}") == upper_index
 
